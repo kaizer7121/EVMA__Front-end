d4984b738cb1dcc802a59a48d2e7217ecdde6688
unknown <daohuuduc.910@gmail.com>
2021-09-21 21:33:35 +0700

    Create detail event page - Add css and responsive

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import { Switch, Route, Redirect } from "react-router-dom";
 import NavigationBar from "./components/Navigation/NavigationBar";
 import SideNavigation from "./components/Navigation/SideNavigation";
 import SignIn from "./components/Auth/SignIn";
-
-import "./App.scss";
+import ListEvent from "./components/Events/ListEvent";
 import SignUp from "./components/Auth/SignUp";
 import ForgotPassword from "./components/Auth/ForgotPassword";
+import EventDetail from "./components/Events/EventDetail";
+
+import "./App.scss";
+
 
 function App() {
   return (
@@ -14,6 +17,12 @@ function App() {
       <Route path="/home">
         <NavigationBar />
         <SideNavigation />
+        <ListEvent />
+      </Route>
+      <Route path="/detail">
+        <NavigationBar />
+        <SideNavigation />
+        <EventDetail />
       </Route>
       <Route path="/sign-in">
         <SignIn />
---
+++ src/Components/Events/Event.js
@@ -0,0 +1,37 @@
+import { Link } from "react-router-dom";
+
+import styles from "./Event.module.scss";
+
+const Event = (props) => {
+  const shortDescriptionArr = props.shortDescription.split("\n");
+
+  return (
+    <section className={`${styles.event}`}>
+      <h1 className={`${styles.event__title}`}>{props.title}</h1>
+      <div className={`${styles.event__information}`}>
+        <div className={`${styles.event__detail}`}>
+          <div>
+            <h3 className={`${styles.event__topic}`}>Short description: </h3>
+            {shortDescriptionArr.map((sentence) => (
+              <p className={`${styles.event__shortDescription}`}>{sentence}</p>
+            ))}
+          </div>
+          <div>
+            <h3 className={`${styles.event__topic}`}>Categories: </h3>
+            {props.categories.map((category) => (
+              <p className={`${styles.event__category}`}>{category}</p>
+            ))}
+          </div>
+          <Link to="/detail" className={`${styles.event__btnText}`}>
+            Learn more &rarr;
+          </Link>
+        </div>
+        <div className={`${styles.event__poster}`}>
+          <img src={props.image} alt="Poster" />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Event;
