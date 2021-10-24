5cf422b4a79b191c2fa8116314e8de455b6372b6
unknown <daohuuduc.910@gmail.com>
2021-10-24 16:11:33 +0700

    Change auth page - only sign in with google

--- src/Components/Auth/SignIn.js
+++ src/Components/Auth/SignIn.js
@@ -12,31 +12,22 @@ import { tokenAction } from "../../Store/tokenSlice";
 import { useSelector } from "react-redux";
 
 const SignIn = () => {
-  const [userInfo, setUserInfo] = useState({
-    email: "",
-    password: "",
-  });
-  const [errorInfo, setErrorInfo] = useState({
-    email: false,
-    password: false,
-    wrongInfo: false,
-  });
-  const [rememberUser, setRememberUser] = useState(false);
   const [isWaiting, setIsWaiting] = useState(false);
-  // const [isHaveGoogleToken, setIsHaveGoogleToken] = useState(false);
+  const [isHaveGoogleToken, setIsHaveGoogleToken] = useState(false);
   const token = useSelector((state) => state.token.token);
 
   const dispatch = useDispatch();
   const history = useHistory();
 
-  // useEffect(() => {
-  //   const unregisterAuthObserver = firebase
-  //     .auth()
-  //     .onAuthStateChanged((user) => {
-  //       setIsHaveGoogleToken(!!user);
-  //     });
-  //   return () => unregisterAuthObserver(); // Make sure we un-register Firebase observers when the component unmounts.
-  // }, []);
+  useEffect(() => {
+    const unregisterAuthObserver = firebase
+      .auth()
+      .onAuthStateChanged((user) => {
+        setIsHaveGoogleToken(!!user);
+        !!user && setIsWaiting(true);
+      });
+    return () => unregisterAuthObserver(); // Make sure we un-register Firebase observers when the component unmounts.
+  }, []);
 
   useEffect(() => {
     if (token) {
@@ -45,155 +36,35 @@ const SignIn = () => {
     }
   }, [token, history]);
 
-  const inputHanlder = (event) => {
-    const type = event.target.id;
-    const value = event.target.value;
-    setUserInfo((prevValue) => ({ ...prevValue, [type]: value }));
-  };
-
-  const changeRemember = () => {
-    setRememberUser(!rememberUser);
-  };
-
-  const checkValidInfo = () => {
-    let email = false;
-    let password = false;
-    if (!validateEmail(userInfo.email)) {
-      email = true;
-    }
-    if (userInfo.password.length < 8 || userInfo.password.length > 30) {
-      password = true;
-    }
-    setErrorInfo({
-      email,
-      password,
-    });
-
-    return !(email || password);
-  };
-
-  const signInHandler = async (event) => {
-    event.preventDefault();
-    if (checkValidInfo()) {
-      try {
-        setIsWaiting(true);
-        const response = await signIn(userInfo);
-        if (response.status === "Login success") {
-          const { profile, token } = response;
-
-          await signInWithFullImage(profile, dispatch);
-          dispatch(tokenAction.addToken(token));
-
-          if (!rememberUser) {
-            localStorage.setItem("RELOAD_LEFT", 1);
-          }
-          setIsWaiting(false);
-        } else {
-          setIsWaiting(false);
-          setErrorInfo((prevValue) => ({ ...prevValue, wrongInfo: true }));
-        }
-      } catch (error) {
-        setIsWaiting(false);
-        console.log("Fail when signup " + error);
-      }
-    } else {
-      setUserInfo((prevValue) => ({ ...prevValue, password: "" }));
-    }
-  };
-
   return (
     <div className={`${styles.login}`}>
       <div className={`${styles.login__box}`}>
         <div className={`${styles.login__poster}`}></div>
         <div className={`${styles.login__content}`}>
-          <h1>EVMA</h1>
-          <h3>Welcome back to EVMA</h3>
-          <p>
-            First time ? <Link to="/sign-up">Create an account</Link>
-          </p>
-          <form className={`${commonStyles.form}`} onSubmit={signInHandler}>
-            {errorInfo.wrongInfo && (
-              <p className={`${commonStyles.form__error}`}>
-                Username or password not correct
-              </p>
-            )}
-            <div className={`${styles.login__form__group}`}>
-              <label htmlFor="email">Email</label>
-              <input
-                type="text"
-                id="email"
-                className={`${commonStyles.form__input}`}
-                onChange={inputHanlder}
-                value={userInfo.email}
-              />
+          <div className={`${styles.login__content_detail}`}>
+            <div className={`${styles.login__content_detail_title}`}>
+              <h1>EVMA</h1>
+              <h3>Welcome back to EVMA</h3>
             </div>
-            {errorInfo.email && (
-              <p className={`${commonStyles.form__error}`}>
-                Your email format must be correct
-              </p>
-            )}
-            <div className={`${styles.login__form__group}`}>
-              <label htmlFor="password">
-                Password <Link to="/forgot-password">Forgot password ?</Link>
-              </label>
-              <input
-                type="password"
-                id="password"
-                className={`${commonStyles.form__input}`}
-                onChange={inputHanlder}
-                value={userInfo.password}
-              />
-            </div>
-            {errorInfo.password && (
-              <p className={`${commonStyles.form__error}`}>
-                Your password must have 8-30 characters
-              </p>
-            )}
 
-            <div className={`${styles.login__form__checkbox}`}>
-              <input
-                type="checkbox"
-                id="remember"
-                value={rememberUser}
-                onChange={changeRemember}
+            {isWaiting && <div className={`${commonStyles.loader_icon_big} ${styles.login__content_detail_button}`}></div>}
+            {!isWaiting && <div
+              className={`${commonStyles.googleBtn} ${styles.login__content_detail_button}`}
+            >
+              <StyledFirebaseAuth
+                uiConfig={uiConfig}
+                firebaseAuth={firebase.auth()}
               />
-              <label htmlFor="remember">Remember Me</label>
+            </div>}
+            
+            <div className={`${styles.login__content_detail_text}`}>
+              <p>Feel free to join with us</p>
+              <span
+                className={`${styles.login__content_detail_text_copyRight}`}
+              >
+                &copy; 2021 by Harry. All rights reserved
+              </span>
             </div>
-            {!isWaiting && (
-              <button
-                type="submit"
-                className={`${commonStyles.btn} ${commonStyles.btn_primary}`}
-              >
-                Sign In
-              </button>
-            )}
-            {isWaiting && (
-              <button
-                type="button"
-                className={`${commonStyles.btn} ${commonStyles.btn_wait} ${commonStyles.btn_grey_dark}`}
-              >
-                <div className={commonStyles.loader_icon}></div>
-                Create An Account
-              </button>
-            )}
-          </form>
-          <div className={`${commonStyles.separator}`}>OR</div>
-          {/* <button
-            type="submit"
-            className={`${commonStyles.btn} ${commonStyles.btn_primary_outline}`}
-          >
-            <img
-              src="/images/google-icon.png"
-              alt="Google icon"
-              className={`${commonStyles.icon}`}
-            />
-            Google
-          </button> */}
-          <div className={commonStyles.googleBtn}>
-            <StyledFirebaseAuth
-              uiConfig={uiConfig}
-              firebaseAuth={firebase.auth()}
-            />
           </div>
         </div>
       </div>
--- src/Components/Auth/SignUp.js
+++ src/Components/Auth/SignUp.js
@@ -13,21 +13,14 @@ import { formatDate, parseDate } from "react-day-picker/moment";
 import { signUp } from "../../Service/api/authApi";
 import { useSelector } from "react-redux";
 
-const SignUp = () => {
+const UpdateProfile = () => {
   const [registerInfo, setRegisterInfo] = useState({
-    email: "",
-    password: "",
-    confirmPassword: "",
     fullName: "",
     dateOfBirth: "",
     role: "",
   });
   const [userYearOld, setUserYearOld] = useState("");
   const [errorRegister, setErrorRegister] = useState({
-    email: false,
-    isDuplicateEmail: false,
-    password: false,
-    confirmPassword: false,
     fullName: false,
     dateOfBirth: false,
   });
@@ -58,20 +51,8 @@ const SignUp = () => {
   };
 
   const checkValidInfo = () => {
-    let email = false;
-    let password = false;
-    let confirmPassword = false;
     let fullName = false;
     let dateOfBirth = false;
-    if (!validateEmail(registerInfo.email)) {
-      email = true;
-    }
-    if (registerInfo.password.length < 8 || registerInfo.password.length > 30) {
-      password = true;
-    }
-    if (registerInfo.confirmPassword !== registerInfo.password) {
-      confirmPassword = true;
-    }
     if (!validateName(registerInfo.fullName)) {
       fullName = true;
     }
@@ -79,14 +60,11 @@ const SignUp = () => {
       dateOfBirth = true;
     }
     setErrorRegister({
-      email,
-      password,
-      confirmPassword,
       fullName,
       dateOfBirth,
     });
 
-    return !(email || password || confirmPassword || fullName || dateOfBirth);
+    return !(fullName || dateOfBirth);
   };
 
   const signUpHandler = async (event) => {
@@ -130,138 +108,90 @@ const SignUp = () => {
       <div className={`${styles.register__box}`}>
         <div className={`${styles.register__poster}`}></div>
         <div className={`${styles.register__content}`}>
-          <h1>EVMA</h1>
-          <p>
-            Already have account ? <Link to="/sign-in">Sign in</Link>
-          </p>
+          <div className={`${styles.register__content_detail}`}>
+            <div>
+              <h1>EVMA</h1>
+              <p>Please complete informations bellow</p>
+            </div>
 
-          <form className={`${commonStyles.form}`} onSubmit={signUpHandler}>
-            {errorRegister.isDuplicateEmail && (
-              <p className={`${commonStyles.form__error}`}>
-                Your email is already existed
-              </p>
-            )}
-            <div className={`${styles.register__form__group}`}>
-              <label htmlFor="email">Email</label>
-              <input
-                type="text"
-                id="email"
-                className={`${commonStyles.form__input}`}
-                value={registerInfo.email}
-                onChange={inputHanlder}
-              />
-              {errorRegister.email && (
-                <p className={`${commonStyles.form__error}`}>
-                  Your email format must be correct
-                </p>
-              )}
-            </div>
-            <div className={`${styles.register__form__group}`}>
-              <label htmlFor="password">Password</label>
-              <input
-                type="password"
-                id="password"
-                className={`${commonStyles.form__input}`}
-                value={registerInfo.password}
-                onChange={inputHanlder}
-              />
-              {errorRegister.password && (
-                <p className={`${commonStyles.form__error}`}>
-                  Your password must have 8-30 characters
-                </p>
-              )}
-            </div>
-            <div className={`${styles.register__form__group}`}>
-              <label htmlFor="confirm-password">Confirm Password</label>
-              <input
-                type="password"
-                id="confirmPassword"
-                className={`${commonStyles.form__input}`}
-                value={registerInfo.confirmPassword}
-                onChange={inputHanlder}
-              />
-              {errorRegister.confirmPassword && (
-                <p className={`${commonStyles.form__error}`}>
-                  Your confirm password doesn't the same as password
-                </p>
-              )}
-            </div>
-            <div className={`${styles.register__form__group} `}>
-              <label htmlFor="full-name">Date of birth (older than 16)</label>
-              <div className={`${styles.register__form__group__date_input}`}>
-                <DayPickerInput
-                  format={"DD/MM/yyyy"}
-                  formatDate={formatDate}
-                  parseDate={parseDate}
-                  placeholder=""
-                  onDayChange={(date) => {
-                    if (date === undefined) date = "";
-                    inputDateHandler(date);
-                  }}
-                  value={registerInfo.dateOfBirth}
-                />
+            <form className={`${commonStyles.form}`} onSubmit={signUpHandler}>
+              <div className={`${styles.register__form__group}`}>
+                <label htmlFor="full-name">Full Name</label>
                 <input
                   type="text"
-                  disabled={true}
-                  className={`${styles.register__form__group__date_input_age}`}
-                  value={userYearOld}
+                  id="fullName"
+                  className={`${commonStyles.form__input}`}
+                  value={registerInfo.fullName}
+                  onChange={inputHanlder}
                 />
+                {errorRegister.fullName && (
+                  <p className={`${commonStyles.form__error}`}>
+                    Full name only have characters (3 - 50)
+                  </p>
+                )}
               </div>
+              <div className={`${styles.register__form__group} `}>
+                <label htmlFor="full-name">Date of birth (older than 16)</label>
+                <div className={`${styles.register__form__group__date_input}`}>
+                  <DayPickerInput
+                    format={"DD/MM/yyyy"}
+                    formatDate={formatDate}
+                    parseDate={parseDate}
+                    placeholder=""
+                    onDayChange={(date) => {
+                      if (date === undefined) date = "";
+                      inputDateHandler(date);
+                    }}
+                    value={registerInfo.dateOfBirth}
+                  />
+                  <input
+                    type="text"
+                    disabled={true}
+                    className={`${styles.register__form__group__date_input_age}`}
+                    value={userYearOld}
+                  />
+                </div>
 
-              {errorRegister.dateOfBirth && (
-                <p className={`${commonStyles.form__error}`}>
-                  Date must be correct, age older than 16
-                </p>
+                {errorRegister.dateOfBirth && (
+                  <p className={`${commonStyles.form__error}`}>
+                    Date must be correct, age older than 16
+                  </p>
+                )}
+              </div>
+              <label htmlForfor="cars">Choose a role:</label>
+              <div className={`${styles.register__form__group}`}>
+                <select
+                  id="role"
+                  className={`${commonStyles.form__input} ${commonStyles.form__input_large}`}
+                  onClick={inputHanlder}
+                >
+                  <option value="Attendees">Attendees</option>
+                  <option value="Event Organizer">Event Organizer</option>
+                </select>
+              </div>
+              {!isWaiting && (
+                <button
+                  type="submit"
+                  className={`${commonStyles.btn} ${commonStyles.btn_primary}`}
+                >
+                  Update Profile
+                </button>
               )}
-            </div>
-            <label htmlForfor="cars">Choose a role:</label>
-            <div className={`${styles.register__form__group}`}>
-              <select
-                id="role"
-                className={`${commonStyles.form__input} ${commonStyles.form__input_large}`}
-                onClick={inputHanlder}
-              >
-                <option value="Attendees">Attendees</option>
-                <option value="Event Organizer">Event Organizer</option>
-              </select>
-            </div>
-            <div className={`${styles.register__form__group}`}>
-              <label htmlFor="full-name">Full Name</label>
-              <input
-                type="text"
-                id="fullName"
-                className={`${commonStyles.form__input}`}
-                value={registerInfo.fullName}
-                onChange={inputHanlder}
-              />
-              {errorRegister.fullName && (
-                <p className={`${commonStyles.form__error}`}>
-                  Full name only have characters (3 - 50)
-                </p>
+              {isWaiting && (
+                <button
+                  type="button"
+                  className={`${commonStyles.btn} ${commonStyles.btn_wait} ${commonStyles.btn_grey_dark}`}
+                >
+                  <div className={commonStyles.loader_icon}></div>
+                  Update Profile
+                </button>
               )}
-            </div>
-            {!isWaiting && (
-              <button
-                type="submit"
-                className={`${commonStyles.btn} ${commonStyles.btn_primary}`}
-              >
-                Create An Account
-              </button>
-            )}
-            {isWaiting && (
-              <button
-                type="button"
-                className={`${commonStyles.btn} ${commonStyles.btn_wait} ${commonStyles.btn_grey_dark}`}
-              >
-                <div className={commonStyles.loader_icon}></div>
-                Create An Account
-              </button>
-            )}
-          </form>
+            </form>
+          </div>
         </div>
       </div>
     </div>
   );
 };
 
-export default SignUp;
+export default UpdateProfile;
