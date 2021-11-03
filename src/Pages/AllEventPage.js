70b4575ddb1ec24c968f2fd916471f14733aa1b7
harry <daohuuduc.910@gmail.com>
2021-11-04 02:02:55 +0700

    Fix bug send too much request when scrolling

--- src/Pages/AllEventPage.js
+++ src/Pages/AllEventPage.js
@@ -11,27 +11,33 @@ const AllEventPage = () => {
     end: false,
   });
   const [isLoading, setIsLoading] = useState(true);
+  const [isGettingNewEvent, setIsGettingNewEvent] = useState(false);
+  const [gottonPage, SetGottenPage] = useState(-1);
 
   const trackScrolling = useCallback(() => {
     const wrappedElement = document.getElementById("header");
 
     if (wrappedElement) {
-      const isBottom =
-        wrappedElement.getBoundingClientRect().bottom * 0.8 <=
-        window.innerHeight;
+      if (!isGettingNewEvent) {
+        const isBottom =
+          wrappedElement.getBoundingClientRect().bottom * 0.75 <=
+          window.innerHeight;
 
-      if (isBottom) {
-        setPagination((prevValue) => ({
-          ...prevValue,
-          page: pagination.page + 1,
-        }));
+        if (isBottom) {
+          setPagination((prevValue) => ({
+            ...prevValue,
+            page: pagination.page + 1,
+          }));
+          setIsGettingNewEvent(true);
+        }
       }
     } else {
       window.removeEventListener("scroll", trackScrolling);
     }
-  }, [pagination.page]);
+  }, [pagination.page, isGettingNewEvent]);
 
   useEffect(() => {
+    window.removeEventListener("scroll", trackScrolling);
     window.addEventListener("scroll", trackScrolling);
 
     return function cleanup() {
@@ -45,6 +51,7 @@ const AllEventPage = () => {
         const params = {
           page: pagination.page,
         };
+        SetGottenPage(pagination.page);
         const response = await getAllEvent(params);
 
         if (response.content && response.content.length === 0) {
@@ -55,15 +62,16 @@ const AllEventPage = () => {
           }));
         }
         setListEvent((prevValue) => [...prevValue, ...response.content]);
+        setIsGettingNewEvent(false);
         setIsLoading(false);
       } catch (err) {
         console.log("Fail when get all event: " + err);
       }
     };
-    if (!pagination.end) {
+    if (!pagination.end && gottonPage < pagination.page) {
       fetchAllEvent();
     }
-  }, [trackScrolling, pagination.end, pagination.page]);
+  }, [trackScrolling, pagination.end, pagination.page, gottonPage]);
 
   useEffect(() => {
     window.scrollTo(0, 0);
